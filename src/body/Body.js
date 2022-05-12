8e802c8e53440e211b0372f05c1de14a1459fbcd
MatthiasVandeCasteele <matthias.van.de.casteele@student.ehb.be>
2022-05-12 16:25:46 +0200

    Card footer all content added, added filter (WIP), styling changes to card, Starting trending container (WIP)
    NEW:
    + Card footer added hover transitions on all elements
    + Card footer added Comment count, share button and Save button (SVG's added)
    + Open body now actually opens the body content
    + Added filter options at top of body (Not functional yet)
    
    CHANGES:
    + Various styling changes to Card (colors, hover animation/transition, drop shadow)
    
    TODO:
    - Make filter functional
    - Fix side-nav text wrapping
    - Add reposts of other reddit posts inside body
    - Complete top navigation
    - Show subreddit icon in posts
    - Add trending banners at the top of the body

--- src/body/Body.js
+++ src/body/Body.js
@@ -4,15 +4,6 @@ import "./Body.css";
 import Card from "./Card.js";
 // API endpoint for main page = https://www.reddit.com/.json
 
-function BodyItem(props) {
-    return (
-        <div className>
-            <p>
-                {props.subreddit}
-            </p>
-        </div>
-    );
-}
 
 export default class Body extends React.Component {
     constructor(props) {
@@ -23,7 +14,22 @@ export default class Body extends React.Component {
         }
     }
 
-    createBox() {
+    filters() {
+        return (
+            <div className="filter-container">
+                <div className="filter-subcontainer">
+                    <p>Popular posts</p>
+                    <p>Hot</p>
+                    <p>Top</p>
+                    <p>All</p>
+                    <p>Trending</p>
+                    <p>Today</p>
+                </div>
+            </div>
+        );
+    }
+
+    createCard() {
         let arr = [];
         for (let i = 0; i < Object.keys(this.state.data).length; i++) {
             arr.push(this.state.data[i].data);
@@ -37,6 +43,11 @@ export default class Body extends React.Component {
                 count++;
                 e.media ? console.log("yes") : console.log("no");
                 let ups = e.ups;
+                let comm_num = e.num_comments;
+                if (comm_num > 1000) {
+                    comm_num = comm_num / 1000;
+                    comm_num = comm_num.toFixed(1);
+                }
                 if (ups > 1000) {
                     ups = ups / 1000;
                     ups = ups.toFixed(1);
@@ -46,7 +57,7 @@ export default class Body extends React.Component {
 
                 return (
                     <div key={count} id="body-content-container">
-                        <Card ups={ups} data={e} subreddit={e.subreddit} user={e.author} contentText={e.selftext_html} contentTitle={e.title} media={e.media} />
+                        <Card ups={ups} num_comments={comm_num} data={e} subreddit={e.subreddit} user={e.author} contentText={e.selftext_html} contentTitle={e.title} media={e.media} />
                     </div>
                 );
             })
@@ -54,25 +65,28 @@ export default class Body extends React.Component {
     }
 
     componentDidMount() {
-        fetch(`https://www.reddit.com/.json`)
-            .then(res => res.json())
-            .then(data => {
-                if (data) {
-                    this.setState({ data: data.data.children });
-                    this.setState({ dataIsLoaded: true });
-                }
-            });
+        if (!this.state.dataIsLoaded) {
+            fetch(`https://www.reddit.com/.json`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data) {
+                        this.setState({ data: data.data.children });
+                        this.setState({ dataIsLoaded: true });
+                    }
+                })
+                .catch(err => console.log(err));
+        }
     }
 
     render() {
         return (
             <div id="body-container" className={this.props.navState} >
-                {this.state.dataIsLoaded ? this.createBox() : <div id="loading-body">
+                <this.filters></this.filters>
+                {this.state.dataIsLoaded ? this.createCard() : <div id="loading-body">
                     <div id="loading-circle-1"></div>
                     <div id="loading-circle-2"></div>
                     <p>loading</p>
                 </div>}
-
             </div>
         );
     }
---
+++ src/body/Trending.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Trending extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div id="trending-container">
+                
+            </div>
+        );
+    }
+} 
